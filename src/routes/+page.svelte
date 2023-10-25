2d739827265eed8ef6afd830bc941f18f8edefec
scotty974 <fabienethevepro@gmail.com>
2023-10-25 22:49:47 +0200

    Ajout des règles et du routing vers la page home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,23 @@
 <svelete:head>
-    <title>Accueil</title>
+  <title>Accueil</title>
 </svelete:head>
 
-
 <section class="container">
-    <div class="row">
-        <h1 class="text-center">Bonjour</h1>
+  <div class="row">
+    <div
+      class="col-12 content d-flex justify-content-center align-items-center"
+    >
+      <div
+        class="d-flex flex-column justify-content-center align-items-center home-content"
+      >
+        <h1 class="text-center">Les règles !</h1>
+        <p>
+          Explorez un univers de super-héros farfelus et humoristiques.
+          Découvrez des bandes dessinées, des histoires courtes et participez à
+          des concours. Préparez-vous à rire !
+        </p>
+        <a href="/home">Commencer</a>
+      </div>
     </div>
+  </div>
 </section>
